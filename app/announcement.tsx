78cdc37f3da61e1e0ab42fd755519b23c2c7dd40
xpan0988 <xiyuanpan86@gmail.com>
2025-03-21 14:43:44 +1100

    Create announcement.tsx

---
+++ app/announcement.tsx
@@ -0,0 +1,8 @@
+export default function Announcement() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Announcement View</h1>
+      <p>This is the announcement page.</p>
+    </main>
+  );
+}
